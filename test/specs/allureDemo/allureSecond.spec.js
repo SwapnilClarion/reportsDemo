02cbb6c1ac39687dd772dd6bfb805479936d9ef6
swapnil.naidu <swapnil.naidu@clariontechnologies.co.in>
2020-04-07 18:09:22 +0530

    Second Test File added

---
+++ test/specs/allureDemo/allureSecond.spec.js
@@ -0,0 +1,17 @@
+const expect=require('chai').expect;
+
+describe('Check Allure report', () => {
+    it('Test-1:Failed Test ', () => {
+      expect(true).to.be.false;
+    });
+  
+    it('Test-2: Passed Test ', () => {
+      expect(true).to.be.true;
+    });
+  
+    it.skip('Test-3: Skipped Test ', () => {
+      expect(true).to.be.false;
+    });
+  
+});
+  
